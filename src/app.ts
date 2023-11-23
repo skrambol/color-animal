14f0d64385c71e07c0ad0cd4d3748b54b122d26b
skrambol <systemplado@gmail.com>
2023-11-23 11:10:57 +0800

    added morgan for logging

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
+import morgan from "morgan"
 import tupleRouter from "./controllers/tuple";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.use("/tuples", tupleRouter);
 
